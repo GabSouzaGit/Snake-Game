4204a3fd2036e5766527e47f4589efdc8a151413
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2023-11-18 18:28:01 -0300

    Documentando função "updateSnake"

--- js/functions/gameFunctions/movimentation/updateSnake.js
+++ js/functions/gameFunctions/movimentation/updateSnake.js
@@ -1,5 +1,12 @@
 import snake from "../../../elements/snake.js";
 
+/**
+ * @description Atualiza a matriz da snake, trocando a posição das coordenadas e direções.
+ * @param {number} x Nova posição X (horizontal) do objeto.
+ * @param {number} y Nova posição Y (vertical) do objeto.
+ * @param {string} direction Nova direção na qual o objeto será movido.
+ */
+
 function updateSnake(x, y, direction){
     snake.structure = [...snake.structure.slice(1), {direction, x, y}];
 }
